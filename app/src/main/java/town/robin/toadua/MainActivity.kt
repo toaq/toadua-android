c51e2d947ea0ff018bbd856e8791ece326661abb
Robin Townsend <robin@robin.town>
2021-10-26 14:39:39 -0400

    Convert MainActivity to use view binding

--- app/src/main/java/town/robin/toadua/MainActivity.kt
+++ app/src/main/java/town/robin/toadua/MainActivity.kt
@@ -2,10 +2,14 @@ package town.robin.toadua
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import town.robin.toadua.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }

d4b4706a6e2cdef84d5f4bc2d9c443976443994a
Robin Townsend <robin@robin.town>
2021-11-14 13:06:21 -0500

    Clean up auth fragment code

--- app/src/main/java/town/robin/toadua/AuthFragment.kt
+++ app/src/main/java/town/robin/toadua/AuthFragment.kt
@@ -5,91 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import town.robin.toadua.databinding.FragmentAuthBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-private var flagCreateAccount:Boolean = false
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AuthFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AuthFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    private lateinit var myinflate: View
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentAuthBinding
+
+    private enum class AuthType { SIGN_IN, CREATE_ACCOUNT }
+    private var authType = AuthType.SIGN_IN
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        myinflate = inflater.inflate(R.layout.fragment_auth, container, false)
+        binding = FragmentAuthBinding.inflate(inflater, container, false)
 
-        var skipbutton = myinflate.findViewById<Button>(R.id.skip_button)
-        skipbutton.setOnClickListener{
-            Navigation.findNavController(myinflate).navigate(R.id.auth_to_search)
+        binding.continueButton.setOnClickListener {
+            findNavController().navigate(R.id.auth_to_search)
         }
-        /*
-        gotoFragment2 = view.findViewById(R.id.fragment_fragment1_gotofragment2);
-        gotoFragment2.setOnClickListener(this);
-         */
+        binding.skipButton.setOnClickListener {
+            findNavController().navigate(R.id.auth_to_search)
+        }
 
-        var createaccountbutton = myinflate.findViewById<Button>(R.id.create_account_button)
-        var signincreateaccount = myinflate.findViewById<TextView>(R.id.auth_title)
-
-        createaccountbutton.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                // do something
-                if(flagCreateAccount == false) {
-                    flagCreateAccount = true
-                    signincreateaccount.text = getString(R.string.create_account)
-                    createaccountbutton.text = getString(R.string.use_existing_account)
+        binding.createAccountButton.setOnClickListener {
+            when(authType) {
+                AuthType.SIGN_IN -> {
+                    authType = AuthType.CREATE_ACCOUNT
+                    binding.authTitle.text = getString(R.string.create_account)
+                    binding.createAccountButton.text = getString(R.string.use_existing_account)
                 }
-                else if(flagCreateAccount == true) {
-                    flagCreateAccount = false
-                    signincreateaccount.text = getString(R.string.sign_in)
-                    createaccountbutton.text = getString(R.string.create_account)
+                AuthType.CREATE_ACCOUNT -> {
+                    authType = AuthType.SIGN_IN
+                    binding.authTitle.text = getString(R.string.sign_in)
+                    binding.createAccountButton.text = getString(R.string.create_account)
                 }
             }
-        })
+        }
 
-
-        return myinflate
-
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AuthFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AuthFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        return binding.root
     }
 }

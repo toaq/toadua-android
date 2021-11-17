9c501bd7e97630f373cab939fdcd0df8b6326c47
daltarace <daltarace@gmail.com>
2021-11-16 22:58:10 -0500

    menu section of app

--- app/src/main/java/town/robin/toadua/AuthFragment.kt
+++ app/src/main/java/town/robin/toadua/AuthFragment.kt
@@ -5,11 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.navigation.fragment.findNavController
 import town.robin.toadua.databinding.FragmentAuthBinding
+import android.view.MenuItem
+
+
+
 
 class AuthFragment : Fragment() {
     private lateinit var binding: FragmentAuthBinding
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
     private enum class AuthType { SIGN_IN, CREATE_ACCOUNT }
     private var authType = AuthType.SIGN_IN
@@ -42,6 +49,33 @@ class AuthFragment : Fragment() {
             }
         }
 
+
+        //new
+        //setHasOptionsMenu(true)
+       //here
+
         return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setHasOptionsMenu(true)
+        var drawerLayout = binding.myDrawerLayout
+        actionBarDrawerToggle =
+            ActionBarDrawerToggle(activity, drawerLayout, R.string.nav_open , R.string.nav_close)
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+       if (actionBarDrawerToggle?.onOptionsItemSelected(item)) {
+           return true
+        } else {
+           return super.onOptionsItemSelected(item)
+       }
     }
 }
--- app/src/main/java/town/robin/toadua/MainActivity.kt
+++ app/src/main/java/town/robin/toadua/MainActivity.kt
@@ -3,13 +3,20 @@ package town.robin.toadua
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import town.robin.toadua.databinding.ActivityMainBinding
+import android.view.MenuItem
+import android.view.View
+import androidx.annotation.NonNull
+import androidx.appcompat.app.ActionBarDrawerToggle
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
     }
+
 }

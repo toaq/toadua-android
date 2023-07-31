3fb551febc092ac91984a692cbe665e6ecdc3b81
Robin <robin@robin.town>
2023-07-31 14:08:54 -0400

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,36 +9,19 @@ plugins {
 
 android {
     namespace = "town.robin.toadua"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "town.robin.toadua"
-        minSdk = 23
-        targetSdk = 33
-        versionCode = 6
-        versionName = "2.0.0"
-        resourceConfigurations.addAll(
-            listOf(
-                "en",
-                "b+qtq+Latn",
-                "b+jbo",
-                "b+tok",
-                "ja",
-                "b+zh+Hans",
-                "pl",
-                "es",
-                "tl",
-                "fr",
-                "de",
-                "ru",
-                "cs"
-            )
-        )
         manifestPlaceholders += mapOf(
             "apiScheme" to "https",
             "apiHost" to "toadua.uakci.pl",
-            "apiPath" to "/",
+            "apiPath" to "/"
         )
+        applicationId = "town.robin.toadua"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 6
+        versionName = "2.0.0"
     }
 
     buildFeatures {
@@ -80,6 +63,9 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    androidResources {
+        generateLocaleConfig = true
+    }
 }
 
 dependencies {
@@ -92,15 +78,15 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.compose.material3:material3:1.2.0-alpha02")
-    implementation("androidx.compose.animation:animation:1.5.0-beta03")
-    implementation("androidx.compose.ui:ui-tooling:1.5.0-beta03")
-    implementation("androidx.compose.ui:ui:1.5.0-beta03")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.0-beta03")
-    implementation("androidx.compose.material:material-icons-extended:1.5.0-beta03")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha04")
+    implementation("androidx.compose.animation:animation:1.5.0-rc01")
+    implementation("androidx.compose.ui:ui-tooling:1.5.0-rc01")
+    implementation("androidx.compose.ui:ui:1.5.0-rc01")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.0-rc01")
+    implementation("androidx.compose.material:material-icons-extended:1.5.0-rc01")
     implementation("com.google.accompanist:accompanist-flowlayout:0.24.13-rc")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.google.code.gson:gson:2.9.0")
+    implementation("com.google.code.gson:gson:2.10")
     implementation("io.github.z4kn4fein:semver:1.4.2")
 }

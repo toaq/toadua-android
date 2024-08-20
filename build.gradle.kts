449f5a1432adbfb38ce0b0e1ae2890b3562377f1
Robin <robin@robin.town>
2024-08-19 23:08:05 -0400

    Upgrade Gradle stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.2.0-alpha12" apply false
+    id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.22" apply false
     id("com.google.devtools.ksp") version "1.8.22-1.0.11" apply false
 }
